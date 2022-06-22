1fc74e22cc119dd07dfb5a202e1e6c716fe7093f
RaulCruzT <cruztorresraul@gmail.com>
2022-06-21 21:22:36 -0400

    Add Loader

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,10 +1,12 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 import './Detail.css';
-
+import Loader from '../Loader';
 
 const Detail = () => {
     const { state: { base64URL, dropzonefile } } = useLocation();
+    const [isLoaded, setIsLoaded] = useState(true);
 
     const toObject = (file) => {
         return URL.createObjectURL(file)
@@ -14,35 +16,39 @@ const Detail = () => {
 
     return(
         <div className="detail">
-            <div className="card-2">
-                <div className="card-image-container">
-                    <img className="card-image" src={toObject(dropzonefile)} alt="" />
+            {isLoaded ?
+                <div className="card-2">
+                    <div className="card-image-container">
+                        <img className="card-image" src={toObject(dropzonefile)} alt="" />
+                    </div>
+                    <div className="card-body-container">
+                        <div className="kind">
+                            <h2>Perro</h2>
+                        </div>
+                        <div className="breed1">
+                            <h3>Chihuahua 60%</h3>
+                        </div>
+                        <div className="breed2">
+                            <div>Beagle</div>
+                            <div>10%</div>
+                        </div>
+                        <div className="breed3">
+                            <div>Golden</div>
+                            <div>10%</div>
+                        </div>
+                        <div className="breed4">
+                            <div>San Bernardo</div>
+                            <div>10%</div>
+                        </div>
+                        <div className="breed5">
+                            <div>Pastor Aleman</div>
+                            <div>10%</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="card-body-container">
-                    <div className="kind">
-                        <h2>Perro</h2>
-                    </div>
-                    <div className="breed1">
-                        <h3>Chihuahua 60%</h3>
-                    </div>
-                    <div className="breed2">
-                        <div>Beagle</div>
-                        <div>10%</div>
-                    </div>
-                    <div className="breed3">
-                        <div>Golden</div>
-                        <div>10%</div>
-                    </div>
-                    <div className="breed4">
-                        <div>San Bernardo</div>
-                        <div>10%</div>
-                    </div>
-                    <div className="breed5">
-                        <div>Pastor Aleman</div>
-                        <div>10%</div>
-                    </div>
-                </div>
-            </div>
+                :
+                <Loader />
+            }
         </div>
     );
 }
---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+import "./Loader.css"
+
+const Loader = () => {
+    return(
+        <div class="loader">
+            <div class="outer"></div>
+            <div class="middle"></div>
+            <div class="inner"></div>
+        </div>
+    );
+}
+
+export default Loader;
