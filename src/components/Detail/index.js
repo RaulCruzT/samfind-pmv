ccbc4b8dbdfff2fa62624d8f02c1eb7a7755212a
Pablo Estay <pablo.estay@sansano.usm.cl>
2022-06-22 14:40:35 -0400

    Resuelto llamadas apis para caso local

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -9,6 +9,8 @@ const Detail = () => {
     const { state: { base64URL, dropzonefile } } = useLocation();
     const [isLoaded, setIsLoaded] = useState(false);
     const [apiResponse, setApiResponse] = useState();
+    const [razas, setRazas] = useState();
+    const [porcentajes, setP] = useState();
 
     const toObject = (file) => {
         return URL.createObjectURL(file)
@@ -19,22 +21,24 @@ const Detail = () => {
         let imgFormat = dropzonefile.name.match(/.(?=png|jpg|gif|jpeg)('|"|)\w+/g)[0];
         let imgName = dropzonefile.name.replace(imgFormat,'');
         let imgFormat2 = imgFormat.replace('.','');
-
-        // axios.post(endPoint, {
-        //     image: base64URL,
-        //     name: imgName,
-        //     format: imgFormat2,
-        //     })
-        //     .then(async response => {
-        //         let data2 = await response.data;
-        //         console.log(data2);
-        //         setApiResponse(data2);
-        //         setIsLoaded(true);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //     });
-    });
+        axios.post(endPoint, {
+             image: JSON.stringify(base64URL),
+             name: imgName,
+             format: imgFormat2,
+             })
+             .then(async response => {
+                 let data2 = await response.data;
+                 console.log(data2);
+                 setApiResponse(data2);
+                 console.log(data2.raza);
+                 setRazas(Object.keys(data2.raza));
+                 setP(Object.values(data2.raza))
+                 setIsLoaded(true);
+             })
+             .catch((error) => {
+                 console.log(error);
+             });
+    }, []);
 
     return(
         <div className="detail">
@@ -45,26 +49,26 @@ const Detail = () => {
                     </div>
                     <div className="card-body-container">
                         <div className="kind">
-                            <h2>Perro</h2>
+                            <h2>{ apiResponse.animal }</h2>
                         </div>
                         <div className="breed1">
-                            <h3>Chihuahua 60%</h3>
+                            <h3>{razas[0]} - {(porcentajes[0]*100).toFixed(0)}%</h3>
                         </div>
                         <div className="breed2">
-                            <div>Beagle</div>
-                            <div>10%</div>
+                            <div>{razas[1]}</div>
+                            <div>{(porcentajes[1]*100).toFixed(0)}%</div>
                         </div>
                         <div className="breed3">
-                            <div>Golden</div>
-                            <div>10%</div>
+                            <div>{razas[2]}</div>
+                            <div>{(porcentajes[2]*100).toFixed(0)}%</div>
                         </div>
                         <div className="breed4">
-                            <div>San Bernardo</div>
-                            <div>10%</div>
+                            <div>{razas[3]}</div>
+                            <div>{(porcentajes[3]*100).toFixed(0)}%</div>
                         </div>
                         <div className="breed5">
-                            <div>Pastor Aleman</div>
-                            <div>10%</div>
+                            <div>{razas[4]}</div>
+                            <div>{(porcentajes[4]*100).toFixed(0)}%</div>
                         </div>
                     </div>
                 </div>
